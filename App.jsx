3b0a4a035c71be8c02659c9a22623aa0cfefe44e
Derro950 <aradiderick254@gmail.com>
2025-07-08 00:22:43 +0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Dragon News 🐉📰</h1>
+      <p>Stay updated with the latest blogs!</p>
+    </div>
+  );
+}
+
+export default App;
